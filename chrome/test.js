b3b9199b09b44b7b392510cd1f19c72173ab0a61
Avery Durrant <Avery246813579@gmail.com>
2017-02-25 00:57:58 -0500

    Fixed the overlay issue

--- chrome/test.js
+++ chrome/test.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+
     var TEST = document.getElementById('TEST');
     TEST.innerHTML = "ADSADS";
     localStorage['TEST'] = JSON.stringify({DOG: "MEAT"});
@@ -8,5 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function test() {
         console.log("TEST");
+    }
+
+    document.getElementById('DD').onclick = function(){
+        //chrome.tabs.update({
+        //    url: "http://www.example.com/"
+        //});
+
+        var notification = new Notification('Notification title', {
+            icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+            body: "Hey there! You've been notified!",
+        });
     }
 });
